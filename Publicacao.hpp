403bd90877755aaca7327da6978601c0757922bd
gabrielbckr <gabriel@etac.lan>
2018-11-13 19:55:23 -0200

    espessificacao

---
+++ Publicacao.hpp
@@ -0,0 +1,21 @@
+#ifndef _PUBLIC_LIBRY_
+#define _PUBLIC_LIBRY_
+
+#include <string>
+using namespace std;
+
+class Publicacao{
+    int codPublicacao, ano;
+    string titulo, editora; 
+public: 
+    Publicacao(string t, string ed, int a, int cod):
+                titulo(t), editora(ed),ano(a), codPublicacao(cod){};
+    ~Publicacao();
+};
+
+/*  ESPESSIFICAÇÃO */
+
+/*Classe Publicação:
+§ Atributos: codPublicacao (int), titulo (string), editora (string) e ano (int);*/
+
+#endif
