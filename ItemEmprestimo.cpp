56d764140592f3fed6b78b9605cc6dad252b7007
Ivo Capanema <ivoliveirabh@hotmail.com>
2018-12-01 13:33:34 -0200

    inicio dev ItemEmprestimo
    correcao retorno pegarDataDev 2

---
+++ ItemEmprestimo.cpp
@@ -0,0 +1,9 @@
+#include "ItemEmprestimo.hpp"
+
+void ItemEmprestimo::atualizarDataDev(const string& Data) {
+
+}
+const Date ItemEmprestimo::pegarDataDev() const {
+    return "";
+}
+ItemEmprestimo::~ItemEmprestimo(){}
--- ItemEmprestimo.hpp
+++ ItemEmprestimo.hpp
@@ -3,12 +3,15 @@
 
 #include "Date.hpp"
 #include "Livro.hpp"
+#include <string>
 
 class ItemEmprestimo {
     Date dataDevolucao; // data da devolucao
     Livro livro; // livro emprestado
   public:
-    ItemEmprestimo(Livro); // constroi um ItemEmprestimo
+    ItemEmprestimo(const Livro& L) : livro(L), dataDevolucao("01/01/1900") {}; // constroi um ItemEmprestimo
+    void atualizarDataDev(const string& Data);
+    const Date pegarDataDev() const;
     ~ItemEmprestimo(); // destroi o ItemEmprestimo
 };
 
