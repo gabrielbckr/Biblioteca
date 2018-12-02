557eefd7fce24cd0b57062d50d5c0a9bfc05554e
Ivo Capanema <ivoliveirabh@hotmail.com>
2018-12-02 13:24:12 -0200

    correcao ordem de inicializacao
    construtor inicializava os atributos na ordem errada

--- ItemEmprestimo.hpp
+++ ItemEmprestimo.hpp
@@ -9,7 +9,7 @@ class ItemEmprestimo {
     Date dataDevolucao; // data da devolucao
     Livro livro; // livro emprestado
   public:
-    ItemEmprestimo(const Livro& L) : livro(L), dataDevolucao("01/01/1900") {}; // constroi um ItemEmprestimo
+    ItemEmprestimo(const Livro& L) : dataDevolucao("01/01/1900"), livro(L) {} // constroi um ItemEmprestimo
     void atualizarDataDev(const string& Data = ""); // atualiza data de devolucao (default = data de hoje)
     const Date pegarDataDev() const; // retorna data de devolucao
     ~ItemEmprestimo(); // destroi o ItemEmprestimo
