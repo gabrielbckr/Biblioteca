1ef125e4d613d10ab7e24eb0315b1b33f78d1f61
gabrielbckr <gabriel@etac.lan>
2018-12-05 01:28:06 -0200

    class testing

--- testMyVector.cpp
+++ testMyVector.cpp
@@ -3,15 +3,28 @@
 #include <vector>
 using namespace std;
 
-
+class gato{
+    string name;
+public:
+    gato(string n): name(n){};
+    gato(gato& g): name(g.name){};
+    bool operator==(gato& g){
+        return this->name==g.name;
+    }
+    friend  ostream operator<<(ostream& o, gato& g){
+        o<<g.name;
+    }
+    string nome(){
+        return this->name;
+    }
+};
 
 int main(){
-    myVector<double> vec;
-    
-    for (int ii = 0; ii<10; ii++)
-        vec.push_back(ii*10);
-    double foudenr  = 40;
-    double* var = vec.find(foudenr);
-    cout<<*var;
+    myVector<gato> vec;
+    vec.push_back(gato("chato"));
+    vec.push_back(gato("aroldo"));
+    gato foudenr("aroldo");
+    gato* var = vec.find(foudenr);
+    cout<<var->nome();
 
 }
