5a0ca65621044e61025979becf44d2719516e718
gabrielbckr <gabriel@etac.lan>
2018-12-05 17:54:46 -0200

    fix operator=

--- Livro.cpp
+++ Livro.cpp
@@ -12,8 +12,10 @@ int Livro::obterNumExemplares(){
     return qtdeExemplares;   
 }
 Livro& Livro::operator=(Livro& L){
+    this->Publicacao::operator=(L);
     autores = L.autores;
     qtdeExemplares = L.qtdeExemplares;
+    return *this;
 }
 bool Livro::operator==(Livro& L){
     if (autores != L.autores){return false;}
