1708e1a3066d78344c5f76916fb6b9c672e94e37
gabrielbckr <gabriel@etac.lan>
2018-12-08 15:58:07 -0200

    starting biblioteca

--- Biblioteca.cpp
+++ Biblioteca.cpp
@@ -1,11 +1,22 @@
 #include "Biblioteca.hpp"
 #include <fstream>
 using namespace std;
+
 Biblioteca& Biblioteca::arquivarConteudo(string& nome){
     fstream arquivo;
-    arquivo.open(nome, fstream::in | fstream::trunc);
+    arquivo.open(nome, fstream::out | fstream::trunc);
     arquivo<<usuarios;
     arquivo<<livros;
     arquivo<<emprestimos;
+    return *this;
+}
+
+
+Biblioteca& Biblioteca::lerArquivo(string& nome){
+    fstream arquivo;
+    arquivo.open(nome, fstream::in | fstream::trunc);
+    arquivo>>usuarios;
+    arquivo>>livros;
+    arquivo>>emprestimos;
     return *this;
 }
