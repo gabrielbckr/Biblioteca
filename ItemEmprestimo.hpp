135dfdb2990482b58ee15ca388d15b6206c81598
Ivo Capanema <ivoliveirabh@hotmail.com>
2018-12-10 01:35:12 -0200

    emprestimo terminado (?)
    testes pendentes

--- ItemEmprestimo.hpp
+++ ItemEmprestimo.hpp
@@ -8,14 +8,16 @@
 
 class ItemEmprestimo {
     Date dataDevolucao; // data da devolucao
-    Livro livro; // livro emprestado
+    Livro& livro; // referência para o livro emprestado
   public:
-    ItemEmprestimo(const Livro& L = Livro()) : dataDevolucao("01/01/1900"), livro(L) {} // constroi um ItemEmprestimo
+    ItemEmprestimo(Livro& L = Livro()) : dataDevolucao("01/01/1900"), livro(L) {} // constroi um ItemEmprestimo
     void atualizarDataDev(const string& Data = ""); // atualiza data de devolucao (default = data de hoje)
     const Date pegarDataDev() const; // retorna data de devolucao
     bool operator== (const ItemEmprestimo& IE);
     bool isEqualTo (const ItemEmprestimo& IE);
     ItemEmprestimo& operator= (const ItemEmprestimo& IE);
+    ItemEmprestimo& operator++(int){livro++; return *this;}
+    ItemEmprestimo& operator--(int){livro--; return *this;}
     friend std::ostream& operator<< (std::ostream& out, const ItemEmprestimo& IE);
     friend std::istream& operator>> (std::istream& in, ItemEmprestimo& IE);
     ~ItemEmprestimo(); // destroi o ItemEmprestimo
