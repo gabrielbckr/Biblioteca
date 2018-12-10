cda1a0b98d6701780ed6985af39cbd6c456b1746
gabrielbckr <gabriel@etac.lan>
2018-12-10 15:42:54 -0200

    getter methods corrections

--- Biblioteca.hpp
+++ Biblioteca.hpp
@@ -25,18 +25,18 @@ public:
     Biblioteca& excluirEmprestimo(const Emprestimo);
     Biblioteca& exluirItemEmprestimo( Emprestimo, ItemEmprestimo);
     myVector<Usuario>& obterUsuarios();
-    myVector<Publicacao*>& obterPublicacoes();
-    myVector<Livro>& obterLivros();
-    myVector<Periodico>& obterPeriodicos();
-    myVector<Emprestimo>& obterEmprestimos();
+    myVector<Publicacao*> obterPublicacoes();
+    myVector<Livro>& obterLivros() ;
+    myVector<Periodico>& obterPeriodicos() ;
+    myVector<Emprestimo>& obterEmprestimos() ;
     Biblioteca& arquivarConteudo(string&);
     Biblioteca& lerArquivo(string&);
     Biblioteca& inserirItemEmprestimo(Emprestimo, ItemEmprestimo);
     Biblioteca& excluirPublicacao(Publicacao*); // Se a publicação for um livro, este não pode ser excluído se existir algum empréstimo para ele;
     Biblioteca& devolverItem(Emprestimo, Livro);
     Biblioteca& devolverTodosItens(Emprestimo);
-    myVector<Publicacao*>& buscaPublicacao(const string&);
-    myVector<Livro>& buscaLivroAutor(const string&);
+    myVector<Publicacao*> buscaPublicacao(const string&);
+    myVector<Livro> buscaLivroAutor(const string&);
 };
 
 #endif
